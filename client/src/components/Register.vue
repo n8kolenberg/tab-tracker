2e01d46907b479949ccfcd52fc26177993d62325
Nathaniel Kolenberg <nathanielkolenberg@Nathaniels-MBP.home>
2019-02-04 21:52:36 +0100

    Add Register component and view to allow users to register

---
+++ client/src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <input 
+    type="email"
+    name="email"
+    placeholder="email"
+    v-model="email"
+    >
+    <input 
+    type="password"
+    name="password"
+    placeholder="password"
+    v-model="password"
+    >
+    <button
+    class="btn btn-medium btn-primary"
+    @click="register"
+    >Register</button>
+  </div>
+  </div>
+</template>
+
+<script>
+
+// @ is an alias to /src
+import Authentication from "../../services/AuthenticationServices";
+export default {
+  email: "",
+  password: "",
+  methods: {
+     async register () {
+        console.log({
+            "email": this.email,
+            "pw": this.password
+        });
+         const response = await Authentication.register({
+              email: this.email,
+              password: this.password
+          });
+          console.log(response.data); 
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <Register/>
+  </div>
+</template>
+
+<script>
+import Register from '@/components/Register'
+export default {
+	name: 'register',
+	components: {
+			Register
+	}
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
